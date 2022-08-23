fee3c1254d5c1167e4937cda1540b8f8f93a7bd1
facundoborrazas <hernanborrazas@gmail.com>
2022-08-23 19:00:30 -0300

    V14
    Intento de arreglo del tema de google

--- js/login.js
+++ js/login.js
@@ -55,22 +55,29 @@ en el caso negativo nos recargará la pantalla de logeo*/
 /*Google:*/
 
 function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token);
-}
+    console.log("ID Token: " + id_token);
+  }
 
 googleS.addEventListener('click', function () { /*En la constante googleS agregamos el evento "click" 
 para simular que se ha podido ingresar con google y nos muestre un mensaje de aprovacion
  si hicimos bien el logeo.*/
+ if(onSignIn(googleUser)){
     setTimeout(() => {
         showAlertSuccess();
     }, 5000);
     setTimeout(() => {
         location.href = "index.html"
     }, 7000);
+ }
 });
